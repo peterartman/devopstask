a105703a5967cdb0c653b4cbba1a47decd740eb9
Chris Meudec <echancrure@gmail.com>
2023-11-26 21:42:31 +0000

    Upgraded to JUnit 5

--- studentTest.java
+++ studentTest.java
@@ -1,86 +1,93 @@
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 /**
  * Created by meudecc on 15/02/2017.
  */
-public class studentTest {
-    @org.junit.Test(expected = IllegalArgumentException.class)
-    public void belowZero() throws Exception {
+public class StudentTest {
+    @Test
+    public void belowZero() {
         Student std = new Student();
-        std.getAttendanceGrade(-1);
+        assertThrows(IllegalArgumentException.class, () -> {
+            std.getAttendanceGrade(-1);
+        });
     }
 
-    @org.junit.Test(expected = IllegalArgumentException.class)
-    public void muchBelowZero() throws Exception {
+    @Test
+    public void muchBelowZero(){
         Student std = new Student();
-        std.getAttendanceGrade(-42);
+        assertThrows(IllegalArgumentException.class, () -> {
+            std.getAttendanceGrade(-42);
+        });
     }
 
-    @org.junit.Test(expected = IllegalArgumentException.class)
-    public void above100() throws Exception {
+    @Test
+    public void above100() {
         Student std = new Student();
-        std.getAttendanceGrade(101);
+        assertThrows(IllegalArgumentException.class, () -> {
+            std.getAttendanceGrade(101);
+        });
     }
 
-    @org.junit.Test(expected = IllegalArgumentException.class)
-    public void muchAbove100() throws Exception {
+    @Test
+    public void muchAbove100() {
         Student std = new Student();
-        std.getAttendanceGrade(142);
+        assertThrows(IllegalArgumentException.class, () -> {
+            std.getAttendanceGrade(142);
+        });
     }
 
-    @org.junit.Test
+    @Test
     public void absent() {
         Student std = new Student();
         assertEquals(Student.AttendanceGrade.ABSENT, std.getAttendanceGrade(0));
     }
-    @org.junit.Test
+
+    @Test
     public void lowVeryPoor() {
         Student std = new Student();
         assertEquals(Student.AttendanceGrade.VERY_POOR, std.getAttendanceGrade(1));
     }
 
-    @org.junit.Test
+    @Test
     public void highVeryPoor() {
         Student std = new Student();
         assertEquals(Student.AttendanceGrade.VERY_POOR, std.getAttendanceGrade(29));
     }
 
-    @org.junit.Test
+    @Test
     public void lowAverage() {
         Student std = new Student();
         assertEquals(Student.AttendanceGrade.AVERAGE, std.getAttendanceGrade(30));
     }
 
-    @org.junit.Test
+    @Test
     public void highAverage() {
         Student std = new Student();
         assertEquals(Student.AttendanceGrade.AVERAGE, std.getAttendanceGrade(69));
     }
 
-    @org.junit.Test
+    @Test
     public void lowGood() {
         Student std = new Student();
         assertEquals(Student.AttendanceGrade.GOOD, std.getAttendanceGrade(70));
     }
 
-    @org.junit.Test
+    @Test
     public void highGood() {
         Student std = new Student();
         assertEquals(Student.AttendanceGrade.GOOD, std.getAttendanceGrade(89));
     }
 
-    @org.junit.Test
+    @Test
     public void lowVeryGood() {
         Student std = new Student();
         assertEquals(Student.AttendanceGrade.VERY_GOOD, std.getAttendanceGrade(90));
     }
 
-    @org.junit.Test
+    @Test
     public void highVeryGood() {
         Student std = new Student();
         assertEquals(Student.AttendanceGrade.VERY_GOOD, std.getAttendanceGrade(100));
     }
-
 }
